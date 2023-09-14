7d62b83e744f12557477e3c68d3983f915ccf225
lixali <48850628+lixali@users.noreply.github.com>
2023-09-14 03:24:24 -0400

    Update get_data_nytimes.py

--- news_process/data/get_data_nytimes.py
+++ news_process/data/get_data_nytimes.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import requests
 import json
 import time
 import random
 
-api_key = "vvfmGQCTjP0bmz8DpXBYSq8kHVD8odrn"
+api_key = ""
 url = "https://api.nytimes.com/svc/archive/v1/{year}/{month}.json"
 
 for year in range(2019, 2023):
@@ -26,33 +25,3 @@ for year in range(2019, 2023):
                 print(f"Error: {response.status_code}")
             time.sleep(random.randint(12, 17))
 
-
-
-=======
-import requests
-import json
-import time
-import random
-
-api_key = "vvfmGQCTjP0bmz8DpXBYSq8kHVD8odrn"
-url = "https://api.nytimes.com/svc/archive/v1/{year}/{month}.json"
-
-for year in range(2019, 2023):
-    month=1
-    while month <13:
-        filename = f"news_{year}_{month}.json"
-        with open(filename, "w") as f:
-            params = {
-                "api-key": api_key
-            }
-            url_cur = url.format(year=year, month=month)
-            print(url_cur)
-            response = requests.get(url_cur, params=params)
-            if response.status_code == 200:
-                data = json.loads(response.text)
-                json.dump(data, f)
-                month+=1
-            else:
-                print(f"Error: {response.status_code}")
-            time.sleep(random.randint(12, 17))
->>>>>>> 0f0e8e7bffd20db1438c43edfd650580b466fdbf
